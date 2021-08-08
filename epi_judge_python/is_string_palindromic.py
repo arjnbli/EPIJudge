79453623012b879b179c0a627f754f2bdb207f21
Arjun <37790028+arjnbli@users.noreply.github.com>
2021-08-07 22:33:55 -0700

    Solve is_string_palindromic

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -3,6 +3,15 @@ from test_framework import generic_test
 
 def is_palindromic(s: str) -> bool:
     # TODO - you fill in here.
+    if len(s) == 0:
+        return True
+    p1 = 0
+    p2 = len(s) - 1
+    while p1 <= p2:
+        if s[p1] != s[p2]:
+            return False
+        p1 += 1
+        p2 -= 1
     return True
 
 
