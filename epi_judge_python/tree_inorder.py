0c17334065680dfc42cdb2512385c51895a67dfc
Arjun <37790028+arjnbli@users.noreply.github.com>
2021-08-07 22:45:14 -0700

    Solve tree_inorder

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -3,10 +3,17 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
-
+#O(n)|O(d)
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
     # TODO - you fill in here.
-    return []
+    return helper(tree, [])
+
+def helper(node, array):
+    if node:
+        helper(node.left, array)
+        array.append(node.data)
+        helper(node.right, array)
+    return array
 
 
 if __name__ == '__main__':
